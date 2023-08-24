e61ca71306f74d92259e6593e755654a55977382
Natarajan-rangasamy <natraj2431@gmail.com>
2023-08-24 17:37:58 +0530

    final touch

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-
-
 const knex = require('knex');
 
 
 const pg = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : 5432,
-    user : 'postgres',
-    password : '123',
-    database : 'facerec'
+    connectionString : process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized:flase },
+    host:process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
   }
 });
 
