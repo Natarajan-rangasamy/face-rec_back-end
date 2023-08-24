e451c8b46bd1297c033afdc489689c04e66af54c
Natarajan-rangasamy <natraj2431@gmail.com>
2023-08-24 17:43:15 +0530

    misscelenious

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const pg = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized:flase },
+    ssl: { rejectUnauthorized:false },
     host:process.env.DATABASE_HOST,
     port: 5432,
     user: process.env.DATABASE_USER,
